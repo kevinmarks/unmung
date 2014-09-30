59d55579929dd5bbff3ba26b012e503f45cef65a
Kevin Marks <kevinmarks@gmail.com>
2014-09-30 15:52:59 -0700

    add flaky ello support

--- unmung.py
+++ unmung.py
@@ -4,6 +4,8 @@ import feedparser
 
 import jinja2
 import webapp2
+import json
+from google.appengine.api import urlfetch
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -28,8 +30,26 @@ class Feed(webapp2.RequestHandler):
             values["entries"]=["no entries"]
         template = JINJA_ENVIRONMENT.get_template('hfeed.html')
         self.response.write(template.render(values))
+
+
+class Ello(webapp2.RequestHandler):
+    def get(self):
+        url = 'https://ello.co/'+self.request.get('ello').strip()+'.json'
+        values = {"url": url, "feed":"no user found","raw":self.request.get('raw')}
+        result = urlfetch.fetch(url)
+        #self.response.write(result.content)
+        if result.status_code == 200:
+            values["feed"] = json.loads(result.content)
+            values["feeds"] = result.content
+            values["feed"]["links"] = values["feed"].get("links","").split()
+            template = JINJA_ENVIRONMENT.get_template('hfeedello.html')
+            self.response.write(template.render(values))
+        else:
+            self.response.write("%i: %s" % (result.status_code,result.content))
+
         
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/feed',Feed),
+    ('/ello',Ello)
 ], debug=True)
